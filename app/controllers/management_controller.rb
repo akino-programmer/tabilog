4ceb1dc69f64a485b92fc4569ab9ed4acea7e4da
akino-programmer <noguchi_a7@yahoo.co.jp>
2019-12-03 17:18:41 +0900

    management-function

--- app/controllers/management_controller.rb
+++ app/controllers/management_controller.rb
@@ -1,6 +1,18 @@
 class ManagementController < ApplicationController
 
   def index
+    @users = User.all
+    @user_id_1_tweet = Tweet.where(user_id: '1').count
+    @user_id_2_tweet = Tweet.where(user_id: '2').count
+    @user_id_3_tweet = Tweet.where(user_id: '3').count
+    @user_id_4_tweet = Tweet.where(user_id: '4').count
+    @user_id_5_tweet = Tweet.where(user_id: '5').count
+
+    @user_id_1_comment = Comment.where(user_id: '1').count
+    @user_id_2_comment = Comment.where(user_id: '2').count
+    @user_id_3_comment = Comment.where(user_id: '3').count
+    @user_id_4_comment = Comment.where(user_id: '4').count
+    @user_id_5_comment = Comment.where(user_id: '5').count
   end
 
 
