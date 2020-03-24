c6f3507b2af85750da55925a2c92953e8bdf925d
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-03-24 20:30:12 +0900

    retry-star-rating

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
+    comment = Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], rate: comment_params[:rate], user_id: current_user.id)
     redirect_to "/tweets/#{comment.tweet.id}"
   end
 
   private
   def comment_params
-    params.permit(:text, :tweet_id)
+    params.permit(:text, :tweet_id, :rate)
   end
 end
